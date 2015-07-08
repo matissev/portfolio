ed2706fb74c9df999b3a6d9061852deadc366964
matissev <m.vrignaud@lecolededesign.com>
2015-07-08 18:37:41 +0200

    removed build/

--- build/js/elements.js
+++
@@ -1,6 +0,0 @@
-/* --------------------- DOM SELECTORS */
-
-worklink = document.getElementById('worklink');
-nav = document.getElementById('nav');
-projects = document.getElementsByClassName('projects');
-navToggleButton = document.getElementById('nav-toggle-button');
