0781e98ce7d9ded59ffdb58caae192f6a791f9a3
matissev <m.vrignaud@lecolededesign.com>
2015-07-27 15:44:26 +0200

    Locals supported and contact page started

---
+++ js/form.js
@@ -0,0 +1,23 @@
+function checkrequired(which) {
+	var pass = true;
+	
+	if (document.images) {		
+		for (i = 0; i < which.length; i++) {		
+			var tempobj = which.elements[i];
+			if (tempobj.name.substring(0,8) === "required") {		
+				if (((tempobj.type === "text" || tempobj.type=="textarea") && tempobj.value === '') || (tempobj.type.toString().charAt(0) === "s" && tempobj.selectedIndex === -1)) {
+					pass = false;
+					break;
+				}
+			}
+		}
+	}
+
+	if (!pass)	{
+		alert("Tous les champs de ce formulaire sont obligatoires, merci de bien vouloir les renseigner");
+		return false;
+
+	} else {
+		return true;
+	}
+}
